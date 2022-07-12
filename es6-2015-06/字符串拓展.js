d3666895e1bfdbb66deb740335e90c43514ed201
zhangxiaojian <zhangxj@tuya.com>
2022-07-12 20:13:05 +0800

    feat: update

--- es6-2015-06/字符串拓展.js
+++ es6-2015-06/字符串拓展.js
@@ -119,5 +119,9 @@ function SaferHTML(templateData) {
   return s;
 }
 
-
+/**
+ * 字符串拓展方法
+ */
+// String.fromCodePoint() § ⇧
+// ES5 提供String.fromCharCode()方法，用于从 Unicode 码点返回对应字符，但是这个方法不能识别码点大于0xFFFF的字符。
 
